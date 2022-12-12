541bc4908b08d72cd4631c9a3c1a3707cf1083b6
Mark Riley <markriley1999@hotmail.co.uk>
2022-12-12 05:19:56 +0000

    Editing user info access

--- JS/app.js
+++ JS/app.js
@@ -23,20 +23,20 @@ $(document).ready(function() {
     
   });
 
-  $.ajax({
-    type: "GET",
-    url: 'https://blue-grass-0915a4c03.2.azurestaticapps.net/.auth/me',
-    success: function (response) {
-        console.log(response);
-        if (response[0].userRoles[0] == "creator"){
-            createUploadSegement();
-        };
-    },
-    error: function () {
-        console.log("Post Failed");
-    }
+//   $.ajax({
+//     type: "GET",
+//     url: 'https://blue-grass-0915a4c03.2.azurestaticapps.net/.auth/me',
+//     success: function (response) {
+//         console.log(response);
+//         if (response[0].userRoles[0] == "creator"){
+//             createUploadSegement();
+//         };
+//     },
+//     error: function () {
+//         console.log("Post Failed");
+//     }
 
-    });
+//     });
 });
 
 //A function to submit a new asset to the REST endpoint 
@@ -96,24 +96,24 @@ function getImages(){
  
 }
 
-function createUploadSegement(responseData){
-    console.log("We made it here, somehow")
-    //Replace the current HTML in that div with a loading message
-    $('#ImageList').html('<div class="spinner-border" role="status"><span class="sr-only"> &nbsp;</span>');
-    var items = [];
-    items.push( "<hr />");
-    items.push("<video width='320' height='240' controls><source src='"+BLOB_ACCOUNT + val["filePath"] +"' type='video/mp4'></video> <br />")
-    items.push( "File : " + val["title"] + "<br />");
-    items.push( "Uploaded by: " + val["producer"] + " (user id: "+val["publisher"]+")<br />");
-    items.push( "<hr />");
+// function createUploadSegement(responseData){
+//     console.log("We made it here, somehow")
+//     //Replace the current HTML in that div with a loading message
+//     $('#ImageList').html('<div class="spinner-border" role="status"><span class="sr-only"> &nbsp;</span>');
+//     var items = [];
+//     items.push( "<hr />");
+//     items.push("<video width='320' height='240' controls><source src='"+BLOB_ACCOUNT + val["filePath"] +"' type='video/mp4'></video> <br />")
+//     items.push( "File : " + val["title"] + "<br />");
+//     items.push( "Uploaded by: " + val["producer"] + " (user id: "+val["publisher"]+")<br />");
+//     items.push( "<hr />");
     
-    $('#ImageList').empty();
-    //Append the contents of the items array to the ImageList Div
-    $( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-    }).appendTo( "#ImageList" );
-}
+//     $('#ImageList').empty();
+//     //Append the contents of the items array to the ImageList Div
+//     $( "<ul/>", {
+//         "class": "my-new-list",
+//         html: items.join( "" )
+//     }).appendTo( "#ImageList" );
+// }
 
 // async function getUserInfo() {
 //     const response = await fetch('/.auth/me');
